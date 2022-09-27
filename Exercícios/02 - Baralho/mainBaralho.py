a65923dac0ae6424c5d9fc47cdab55e25f65dd35
AllaSmithll <allanammancio2004@gmail.com>
2022-09-26 22:12:46 -0300

    baralho finalizado

--- Exercícios/02 - Baralho/mainBaralho.py
+++ Exercícios/02 - Baralho/mainBaralho.py
@@ -1,1 +1,23 @@
+from Baralho import Baralho
 
+def embaralhar():
+    print('')
+
+embaralhar()
+
+baralho = Baralho(True)
+
+print()
+print(baralho)
+print(len(baralho))
+
+input()
+
+print(baralho)
+
+embaralhar()
+
+for i in range(5):
+    print(baralho.retirarCarta())
+
+print('TOTAL DE CARTAS:', len(baralho))
