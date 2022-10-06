40fc69343453757dabf5d80dbc2290f8e6477e27
AllaSmithll <allanammancio2004@gmail.com>
2022-10-06 10:06:20 -0300

    Conta.py

--- Introdução POO/04 - Tratamento de exceções/Prof. Alex escreveu/Conta.py
+++ Introdução POO/04 - Tratamento de exceções/Prof. Alex escreveu/Conta.py
@@ -1,23 +1,15 @@
-class ContaBloqueadaException(Exception):
-    def __init__(self, msg):
-        super().__init__(msg)
-
 class Conta:
     # construtor parametrizado da classe
     def __init__(self, agencia, conta):
         self.agencia = agencia
         self.conta = conta
         self.digito = self.__geraDigito(conta)
-        self.saldo = 0
-        self.bloqueado = False
+        self.__saldo = 1000
     
     # método de instância público
     def sacar(self, valor):
-        if self.bloqueado == True:
-            raise ContaBloqueadaException('Não é possível sacar pois a conta do cliente está bloqueada')
-
-        assert self.saldo - valor >= 0, 'Saldo insuficiente para saque'
-        self.saldo -= valor
+        assert self.__saldo - valor >= 0
+        self.__saldo -= valor
 
     # método de instância privado
     def __geraDigito(self, conta):
@@ -28,17 +20,9 @@ class Conta:
 
     
     def __str__(self):
-        return f'Conta {self.conta}-{self.digito}, Agencia: {self.agencia}, Saldo: {self.saldo}'
+        return f'Conta {self.conta}-{self.digito}, Agencia: {self.agencia}, Saldo: {self.__saldo}'
 
 if __name__ == '__main__':
     c1 = Conta('1010','4561')
     print(c1)
-    try:
-        c1.saldo = 500
-        c1.bloqueado = True
-        c1.sacar(100)
-        print(c1)
-    except AssertionError as ae:
-        print(ae)
-    except ContaBloqueadaException as cbe:
-        print(cbe)
+    c1.sacar(200 )
