933574873cae4bc9c40c18289222aef4c5145b2b
AllanSmithll <allanammancio2004@gmail.com>
2022-10-23 14:57:40 -0300

    Pilha na computação

--- Estruturas de Dados/01 - Pilha/pilhacomputacao.py
+++ Estruturas de Dados/01 - Pilha/pilhacomputacao.py
@@ -1,20 +1,28 @@
-def a():
-    print('1')
-    b()
+# 22/10/2022
 
-def b():
+'''
+Por mais incrível que pareça, ainda vou codar sobre "Recursividade", mas abaixo já é possível um exemplo deste recurso, até porque há um "chamamento entre os métodos
+'''
+
+def a(): # Definimos o método a, que imprimirá o número 1, depois chamar b()
+    print('1')
+    b() 
+
+def b(): # Chama o método b, que imprimirá 2, depois chamar c()
     print("2")
     c()
 
-def c():
+def c():   # Chama o método c, que imprimirá 3, depois que chama d()
     print("3")
     d()
 
-def d():
-    print("4")
+def d():   # Por fim, o método d, que entregará o "print('4\n')" a quem o chamou
+    print("4\n")
 
 
 print("Início")
 a()
 b()
+c()
+d()
 print("Fim")
