9becb30f17e27e0792f3d95b8853a83d1a2b6efa
AllanSmithll <allanammancio2004@gmail.com>
2022-10-23 23:00:55 -0300

    Pilha sequencial e seus métodos

---
+++ Exercícios/Lista de Árvores Binárias/01-altura-arvore.py
@@ -0,0 +1,18 @@
+# 23/10/2022
+'''
+Regra:
+
+No mínimo h + 1, e no máximo 2h + 1 - 1, utilizando uma árvore binária
+
+'''
+
+class No:
+    def __init__(self, carga:any):
+        self.carga = carga
+        self.esq = None
+        self.dir = None
+
+    def __str__(self) -> str:
+        return f"{self.carga}"
+
+No(20)
