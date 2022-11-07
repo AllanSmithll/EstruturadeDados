1b6c04b13aeeab61d5f2b9487a6784358cbaf806
AllanSmithll <allanammancio2004@gmail.com>
2022-11-07 08:10:25 -0300

    Soma dos n primeiros termos

--- Exercícios/Lista de Recursividade/01-lengthstr.py
+++ Exercícios/Lista de Recursividade/01-lengthstr.py
@@ -6,6 +6,7 @@ def lengthStr(str) -> int:
     return 0
   else:
     return 1 + lengthStr(str[1:])  # Se tiver caracter, retorne 1 + o resultado da chamada de lengthStr, que contará a partir do próximo índice
+    # No caso de 1 elemento, a chamada lenghtStr retornará 0
                                    
-string = "Allan"
+string = "A"
 print(lengthStr(string))
---
+++ Exercícios/Lista de Recursividade/07-somanprimeiros.py
@@ -0,0 +1,20 @@
+# 07/11/2022
+# Função recursiva que retorna soma de n primeiros termos
+
+def primeiros_termos(n) -> int:
+    if n == 0:  # Caso base
+        # Se n = 0, retorne 0
+        return 0
+    else:   # Caso não seja igual a 0
+        return n + primeiros_termos(n-1) # aqui é retornado o elemento n somado com a chamada recursiva da função.
+        '''
+        Assim:
+
+            n + (n-1) + (n-2) ... e assim por diante
+        '''
+
+# Principal
+
+print(primeiros_termos(1))
+print(primeiros_termos(2))
+print(primeiros_termos(3))
