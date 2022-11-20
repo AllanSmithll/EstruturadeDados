49300c5640acfdda9be59d29ebc05ff97d3b0fe0
AllanSmithll <allanammancio2004@gmail.com>
2022-11-20 17:01:57 -0300

    Para não deixar errado

--- Exercícios/06 - Pilha Sequencial/questao01.py
+++ Exercícios/06 - Pilha Sequencial/questao01.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     print("A pilha é esta:", p1,)
     print(f'''\nEditor de Pilha do Allan Alves Amancio v1.0\n============================================================\n
     Pilha Selecionada: [{p1}]
-    topo: {p1[:-1:]}
+    topo: {p1[-1]}
     ''')
