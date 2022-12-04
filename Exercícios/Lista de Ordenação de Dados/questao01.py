a7370110b4d8b386caa42ac21742330db1f3b3be
AllaSmithll <allanammancio2004@gmail.com>
2022-12-04 17:11:06 -0300

    Fazer Selection Sort recursivo

--- Exercícios/Lista de Ordenação de Dados/questao01.py
+++ Exercícios/Lista de Ordenação de Dados/questao01.py
@@ -5,13 +5,19 @@
 # 4- Ao final da varredura, o menor elemento estará posicionado na primeira posição
 # 5- Repetir o procedimento para os n-1 elementos restantes (i = 1, 2, 3, ..., n-1)
 
-def selectionSort(array):
-    for i in range(len(array)-1):
-        min = i
-        for j in range(i+1, len(array)):
-            if(array[j] < array[min]):
-                min = j
+def selectionSortRec(array):
+    __executaSelectionSort(array, len(array))
+
+def __executaSelectionSort(array, size):
+    troca = False
+    for i in range(len(array)-1): # Vamos fazer iterações regressivas
+        min = i # O primeiro valor do array será considerado o menor inicialmente
+        for j in range(i+1, len(array)): # vamos fazer a troca real, a partir do i + 1
+            if(array[j] < array[min]): # Se array na posição j, que seria o elemento i + 1, for menor do que o menor atual
+                min = j  # Menor atual recebe j
         array[min], array[i] = array[i], array[min] # troca
 
 arrays = [20, 10, 3, 4, 6, 30, 40, 50, 35, 5]
-selectionSort(arrays)
+print("Array original:", arrays)
+selectionSortRec(arrays)
+print("Array após o Slection Sort:", arrays)
