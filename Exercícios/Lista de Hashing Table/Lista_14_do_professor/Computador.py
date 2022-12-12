a99e73e85f50d6b76d158742afedf355a491ee6d
AllanSmithll <101681192+AllanSmithll@users.noreply.github.com>
2022-12-12 02:29:38 +0000

    Atualizaçõeszinhas

--- Exercícios/Lista de Hashing Table/Lista_14_do_professor/Computador.py
+++ Exercícios/Lista de Hashing Table/Lista_14_do_professor/Computador.py
@@ -1,17 +1,21 @@
 class Computador():
-    def __init__(self,ip:str, hardware:str):
-      self.ip = ip
-      self.hardware = hardware
+  def __init__(self,ip:str, hardware:str):
+    self.ip = ip
+    self.hardware = hardware
 
-    def __str__(self)->str:
-      return self.ip
+  def __str__(self)->str:
+    if self.ip is None:
+      return None
+    if self.ip and self.hardware != None:
+      return f'{self.ip}, {self.hardware}'
 
-    def __eq__(self, outro):
-      return self.ip == outro.ip
+  def __eq__(self, outro):
+    return self.ip == outro.ip
 
-    def __hash__(self)->int:
-      return hash(self.ip) 
-
+  def __hash__(self)->int:
+    return hash(self.ip)
+    
+    # Incrementei um método que mostre o ip  e o valor do ip
 
 if __name__ == '__main__':
     c1 = Computador('192.168.0.1','intel i7 8GB RAM 1TB HD')
--- Exercícios/Lista de Hashing Table/Lista_14_do_professor/mainChaining.py
+++ Exercícios/Lista de Hashing Table/Lista_14_do_professor/mainChaining.py
@@ -1,59 +1,55 @@
 from ChainingHashTable import ChainingHashTable
+from Computador import Computador
 
 # ------------------------------------------------ #
-#  Programa Principal 
+#  Programa Principal: Parte 1
 # ------------------------------------------------ #
 size = int(input("Informe o tamanho da tabela hash desejada: "))
-cht = ChainingHashTable(size)
+# cht = ChainingHashTable(size)
 
-# Armazenando elementos na tabela Hash
-cht.put(12,'alex')
-cht.displayTable()
-input()
+# # Armazenando elementos na tabela Hash
+# cht.put(12,'alex')
+# cht.displayTable()
+# input()
 
-# Aqui, tenta-se inserir a mesma chave. Como a chave já existe, atualiza o valor
-cht.put(12,'alex sandro')
-cht.displayTable()
-input()
+# # Aqui, tenta-se inserir a mesma chave. Como a chave já existe, atualiza o valor
+# cht.put(12,'alex sandro')
+# cht.displayTable()
+# input()
 
-cht.put(31,'nathan')
-cht.displayTable()
-input()
+# cht.put(31,'nathan')
+# cht.displayTable()
+# input()
 
-cht.put(90,'alice')
-cht.displayTable()
-input()
+# print("Valor que tem chave 31: ", cht.get(31))
+# input()
 
-cht.put(28,'matheus')
-cht.displayTable()
-input()
+# cht.put(29, "Allan")
+# cht.displayTable()
+# input()
 
-cht.put(88,'duda')
-cht.displayTable()
-input()
-
-cht.put(40,'naty')
-cht.displayTable()
-input()
-
-cht.put(77,'alessandra')
-cht.displayTable()
-input()
-
-cht.put(26,'dan')
-cht.displayTable()
-input()
-
-cht.put(17,'jessika')
-cht.displayTable()
-input()
+# cht.put(10, "Marcio")
+# cht.displayTable()
+# input()
 
 
-# A chamada ao método get() gera uma exceção se a chave não existir.
-#print('get():', cht.get(111))
+# # A chamada ao método get() gera uma exceção se a chave não existir.
+# #print('get():', cht.get(111))
 
-cht.remove(12)
-cht.displayTable()
+# # A chamada ao método remove() gera uma exceção se a chave não existir.
+# cht.remove(12)
+# cht.displayTable()
 
-# A chamada ao método remove() gera uma exceção se a chave não existir.
-#cht.get(111)
+# # A chamada de __len__() retorna a quantidade de pares chave/valor
+
+# print("\nQuantos pares chave e valor há na Hash Table?", cht.__len__())
+
+# ------------------------------------------------ #
+#  Programa Principal: Parte 2
+# ------------------------------------------------ #
+
+ht = ChainingHashTable(size)
+c1 = Computador('192.168.0.1', 'Intel i7 8GB RAM 1TB HD')
+ht.put(c1, "Meu computador")
+ht.displayTable()
+print(ht.__str__())
