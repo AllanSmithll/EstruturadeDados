19b2f9d9431a4ec0c4ebac64af053048334b80de
allansmithll <allanammancio2004@gmail.com>
2023-07-30 20:41:51 -0300

    Meu insertion sort

--- Estruturas de Dados/08 - Ordenação de Dados/02 - Selection Sort/meu-selection.py
+++ Estruturas de Dados/08 - Ordenação de Dados/02 - Selection Sort/meu-selection.py
@@ -11,3 +11,22 @@ def selection(array):
 array_teste = [10,5,6,7,3,4,2,1,9,8]
 selection(array_teste)
 print(array_teste)
+
+def letraMaisFrequente(p_string:str) -> str:
+    '''
+    \rletraMaisFrequente(p_string) \n
+    \rp_string: string que será analisada pela função \n
+    Função que retorna o caracter que mais aparece na string, exceto os espaços'''
+    caracteres = {}
+    for carac in p_string.strip():
+        if (carac in caracteres and carac != '  '):
+            caracteres[carac] += 1
+        if (carac not in caracteres and carac != ' '):
+            caracteres[carac] = 1
+        else:
+            continue
+    letra_mais_frequente = max(caracteres, key=caracteres.get)
+    return letra_mais_frequente
+
+str = 'This is Elon Musk'
+print(letraMaisFrequente(str))
